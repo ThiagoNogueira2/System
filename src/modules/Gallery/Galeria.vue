3adf7a342a5c0afbff3573a920e2c406d706f365
ThiagoNogueira2 <thiagonogueira_2003@hotmail.com>
2026-02-05 14:11:25 -0300

    wip: Correção Split e Smooth

--- src/modules/Gallery/Galeria.vue
+++ src/modules/Gallery/Galeria.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="-mb-[200px] pt-0 pb-10 lg:pt-30">
+    <div class="-mb-[200px] pt-0 pb-30 lg:pt-30">
         <div class="sticky top-[30vh] -z-10 flex flex-col justify-start items-center font-bold">
             <p class="text-sm tracking-normal" style="color: #1e1e1e;">
                 AS FEATURED IN
@@ -11,11 +11,11 @@
 
    
         <div ref="galleryContainer" class="mx-auto mt-20 mb-32 px-4 lg:px-0 max-lg:max-w-[85vw] lg:max-w-[60vw]">
-            <div class="relative w-full max-lg:pb-[110%] lg:pb-[110%]">
+            <div class="relative w-full max-lg:pb-[90%] lg:pb-[90%]">
                 
              
                 <div 
-                    class="absolute z-[15] max-lg:w-[46%] lg:w-[33.33%] gallery-item" 
+                    class="absolute z-[15] max-lg:w-[42%] lg:w-[33.33%] gallery-item" 
                     :style="{
                         left: '3.78%',
                         top: '-20.57%'
@@ -30,7 +30,7 @@
 
             
                 <div 
-                    class="absolute z-[3] max-lg:w-[44%] max-lg:top-[30%] lg:w-[33.33%] lg:top-[15.29%] gallery-item" 
+                    class="absolute z-[7] max-lg:w-[44%] max-lg:top-[55%] lg:w-[33.33%] lg:top-[30.29%] gallery-item" 
                     :style="{
                         right: '68.22%'
                     }"
@@ -59,7 +59,7 @@
 
                 
                 <div 
-                    class="absolute z-[4] max-lg:w-[45%] max-lg:top-[34%] lg:w-[33.33%] lg:top-[15.43%] gallery-item" 
+                    class="absolute z-[8] max-lg:w-[45%] max-lg:top-[52%] lg:w-[33.33%] lg:top-[30.43%] gallery-item" 
                     :style="{
                         left: '67.98%'
                     }"
@@ -73,9 +73,8 @@
 
           
                 <div 
-                    class="absolute z-[6] max-lg:w-[44%] lg:w-[33.33%] gallery-item" 
+                    class="absolute z-[6] max-lg:w-[44%] max-lg:left-[33.33%] max-md:w-[55%] max-md:left-[20%] lg:w-[33.33%] lg:left-[33.33%] gallery-item" 
                     :style="{
-                        left: '33.33%',
                         top: '-28.57%'
                     }"
                 >
